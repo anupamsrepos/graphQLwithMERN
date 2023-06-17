5377476479538069f479201489d50d7dce32f6ed
Anupam Nath <anupam1007@gmail.com>
2023-06-17 12:08:20 -0700

    GraphQL is connected to MongoDB, empty data set

--- server/index.js
+++ server/index.js
@@ -2,9 +2,15 @@ const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+//connect to database
+connectDB();
 
 app.use(
   "/graphql",
