86357177590c633e5a96733a513ddf90eac79863
Anupam Nath <anupam1007@gmail.com>
2023-06-18 12:29:05 -0700

    Implemented Project details page with client information

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # graphQLwithMERN
+
+This project is based on this Youtuber's video
+
+https://www.youtube.com/watch?v=BcLNfwF04Kw
--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,33 +1,35 @@
-import { FaTrash} from 'react-icons/fa';
-import { useMutation } from '@apollo/client';
-import { DELETE_CLIENT} from '../mutations/clientMutations';
-import { GET_CLIENTS } from '../queries/clientQueries';
+import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { DELETE_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENTS } from "../queries/clientQueries";
 
-export default function ClientRow({client}) {
-const [deleteClient] = useMutation(DELETE_CLIENT, {
-    variables: { id: client.id},
-    //This is to reload data set after delete 
+export default function ClientRow({ client }) {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    //This is to reload data set after delete
     //refetchQueries: [{ query: GET_CLIENTS}], //-- approach 1 - do not use too much
-    update(cache, {data: {deleteClient}}){     //-- approach 2 - use cache provided by Apollo
-        const {clients} = cache.readQuery({query: GET_CLIENTS});
-        cache.writeQuery({
-            query: GET_CLIENTS,
-            data: { clients: clients.filter(client => client.id != deleteClient.id)},
-        });
-    }
-});
+    update(cache, { data: { deleteClient } }) {
+      //-- approach 2 - use cache provided by Apollo
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.filter((client) => client.id != deleteClient.id),
+        },
+      });
+    },
+  });
 
   return (
     <tr>
-        <td> {client.name} </td>
-        <td> {client.email} </td>
-        <td> {client.phone} </td>
-        <td> 
-            <button className="btn btn-danger btn-sm"
-            onClick={deleteClient}>
-                <FaTrash />
-            </button>    
-        </td>
+      <td> {client.name} </td>
+      <td> {client.email} </td>
+      <td> {client.phone} </td>
+      <td>
+        <button className="btn btn-danger btn-sm" onClick={deleteClient}>
+          <FaTrash />
+        </button>
+      </td>
     </tr>
   );
 }
